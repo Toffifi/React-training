923bd1363cc073a5db4cde2ecafca5e472a9651d
Aliaksandra Kavaliova <aliaksandra_kavaliova@epam.com>
2021-04-22 09:45:37 +0300

    init: first setup

---
+++ src/components/App/index.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react';
+import './style.scss';
+
+const App: FC = () => <p>Hello World</p>;
+console.log('object');
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,52 @@
+import path from 'path';
+import ESLintPlugin from 'eslint-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+
+const config: webpack.Configuration = {
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './static',
+            },
+          },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new ESLintPlugin({
+      files: 'src/**/*.tsx',
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+};
+export default config;
